30fa1e056087866843f39c483a44fc41b115e5ce
Smilexing <2664626327@qq.com>
2024-06-08 16:05:43 +0800

    BiChart-后端代码初始化

---
+++ yubi-backend/src/main/java/com/yupi/springbootinit/mapper/PostMapper.java
@@ -0,0 +1,47 @@
+package com.yupi.springbootinit.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yupi.springbootinit.model.entity.Chart;
+import com.yupi.springbootinit.model.entity.Post;
+import com.yupi.springbootinit.model.entity.User;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * 帖子数据库操作
+ *
+ * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
+ * @from <a href="https://yupi.icu">编程导航知识星球</a>
+ */
+public interface PostMapper extends BaseMapper<Post> {
+
+    /**
+     * 查询帖子列表（包括已被删除的数据）
+     */
+    List<Post> listPostWithDelete(Date minUpdateTime);
+
+    /**
+    * @author szdaiyifei
+    * @description 针对表【chart(图表信息表)】的数据库操作Mapper
+    * @createDate 2023-08-17 21:45:54
+    * @Entity com.yupi.springbootinit.model.entity.Chart
+    */
+    interface ChartMapper extends BaseMapper<Chart> {
+
+    }
+
+    /**
+    * @author szdaiyifei
+    * @description 针对表【user(用户)】的数据库操作Mapper
+    * @createDate 2023-08-17 21:45:54
+    * @Entity com.yupi.springbootinit.model.entity.User
+    */
+    interface UserMapper extends BaseMapper<User> {
+
+    }
+}
+
+
+
+
---
+++ yubi-backend/src/main/java/com/yupi/springbootinit/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.yupi.springbootinit.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yupi.springbootinit.model.entity.User;
+
+/**
+* @author szdaiyifei
+* @description 针对表【user(用户)】的数据库操作Mapper
+* @createDate 2023-08-17 21:45:54
+* @Entity com.yupi.springbootinit.model.entity.User
+*/
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
