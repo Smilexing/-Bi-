f075dfae81ba63850fa03946d14de863781b7030
Smilexing <2664626327@qq.com>
2024-06-11 14:15:39 +0800

    BiChart-基于RabbitTemplate测试消息队列收发

---
+++ yubi-backend/src/main/java/com/yupi/springbootinit/bizmq/MyMessageProducer.java
@@ -0,0 +1,25 @@
+package com.yupi.springbootinit.bizmq;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+
+/**
+ * @author Tom Smile
+ * @version 1.0
+ * @description: 测试RabbitTemplate发送消息
+ * @date 2024/6/10 12:57
+ */
+
+@Component
+public class MyMessageProducer {
+
+    @Resource
+    private RabbitTemplate rabbitTemplate;
+
+    public void sendMessage(String exchange, String routingKey, String message) {
+        rabbitTemplate.convertAndSend(exchange, routingKey, message);
+    }
+
+}
